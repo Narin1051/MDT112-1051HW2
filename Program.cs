c83e07117cc126b71e5b99ece48d0f9bfe1dc6e4
Narin1051 <narin.phue@mail.kmutt.ac.th>
2023-05-19 13:36:06 +0700

    2nd commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program{
+    static void Main(string[] args){
+            
+        Console.WriteLine("Enter the Pascal's triangle row number:");
+        int rowNumber;
+
+        do{
+           rowNumber = int.Parse(Console.ReadLine());
+
+            if(rowNumber >= 0 ){
+                
+                break;
+
+            }
+
+            Console.WriteLine("Invalid Pascal's triangle row number.");
+
+        }while(rowNumber < 0 );
+
+        PascalsTriangle(rowNumber);
+
+    }
+        static void PascalsTriangle(int rows){
+
+        for (int i = 0; i <= rows; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(CalculatePascal(i, j) + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static int CalculatePascal(int row, int column)
+    {
+        if (column == 0 || column == row)
+            return 1;
+
+        return CalculatePascal(row - 1, column - 1) + CalculatePascal(row - 1, column);
+    }
+}
